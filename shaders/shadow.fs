4590f3f6c47b2ca41a849041982fa78fc7f77306
SIDD017 <f20180617@goa.bits-pilani.ac.in>
2022-10-12 16:54:49 +0530

    Fix Shadow Acne using a small depth bias

--- shaders/shadow.fs
+++ shaders/shadow.fs
@@ -15,14 +15,14 @@ uniform sampler2D shadowMap;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 
-float ShadowCalculation(vec4 fragPosLightSpace)
+float ShadowCalculation(vec4 fragPosLightSpace, float bias)
 {
 	vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
 	/* Convert to [0,1] range from [-1,1] */
 	projCoords = projCoords * 0.5 + 0.5;
 	float closestDepth = texture(shadowMap, projCoords.xy).r;
 	float currentDepth = projCoords.z;
-	float shadow = currentDepth > closestDepth  ? 1.0 : 0.0;
+	float shadow = currentDepth-bias > closestDepth  ? 1.0 : 0.0;
 
 	return shadow;
 }
@@ -43,9 +43,11 @@ void main()
     float spec = 0.0;
     vec3 halfwayDir = normalize(lightDir + viewDir);  
     spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
-    vec3 specular = spec * lightColor;    
+    vec3 specular = spec * lightColor; 
+	
+	float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
 
-    float shadow = ShadowCalculation(fs_in.FragPosLightSpace);       
+    float shadow = ShadowCalculation(fs_in.FragPosLightSpace, bias);       
     vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
     
     FragColor = vec4(lighting, 1.0);
