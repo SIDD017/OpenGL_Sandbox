7c485b0685199674822858eaa06ddae87874bc9c
SIDD017 <f20180617@goa.bits-pilani.ac.in>
2022-10-12 17:47:58 +0530

    PCF: Percentage Closer Filtering
    Soften shadow edges by sampling surrounding texels from depth map and
    average the result

--- shaders/shadow.fs
+++ shaders/shadow.fs
@@ -22,7 +22,20 @@ float ShadowCalculation(vec4 fragPosLightSpace, float bias)
 	projCoords = projCoords * 0.5 + 0.5;
 	float closestDepth = texture(shadowMap, projCoords.xy).r;
 	float currentDepth = projCoords.z;
-	float shadow = currentDepth-bias > closestDepth  ? 1.0 : 0.0;
+
+	float shadow = 0.0;
+	vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+	for(int x = -1; x <= 1; ++x)
+	{
+		for(int y = -1; y <= 1; ++y)
+		{
+			float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+			shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;        
+		}    
+	}
+	shadow /= 9.0;
+
+	//float shadow = currentDepth-bias > closestDepth  ? 1.0 : 0.0;
 	/* For fragments that fall outside the light space's frustum when viewed in light space. */
 	if(projCoords.z > 1.0)
         shadow = 0.0;
